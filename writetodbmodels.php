b75569d218362371ce4eb5e183c1a01c10744763
KlimovCode <klimovcode@mail.ru>
2021-01-30 10:27:48 +0500

    Update writetodbmodels.php

--- writetodbmodels.php
+++ writetodbmodels.php
@@ -17,10 +17,12 @@ foreach ($brends as $brend) {  // enumerating directories
         $models = scandir('img/' . $brend);
         foreach ($models as $model) {
             if ($model == '.' || $model == '..' || preg_match('/^logo/', $model)) { continue; }
-
             $model_imgs = scandir('img/' . $brend . '/' . $model);
             foreach ($model_imgs as $model_img) {
                 if (!preg_match('/^logo/', $model_img)) { continue; }
+                $model_img = 'img/' . $brend . '/' . $model . '/' . $model_img;
+//                echo $model . '<br>' . $model_img . '<br>' . $id_brend . '<hr>';
+//                $sqldel = "DELETE FROM models";
                 $sql = "INSERT INTO `models` (`id`, `name`, `img`, `id_brend`) VALUES (NULL, '$model', '$model_img', '$id_brend')";
                 if ($link->query($sql) === TRUE) {
                     echo "New record $model created successfully" . '<br>';
