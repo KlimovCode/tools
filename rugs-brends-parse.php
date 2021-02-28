6f47507f8e59a944b2426d3ff5057a99d0f7b724
KlimovCode <klimovcode@mail.ru>
2021-02-28 20:32:22 +0500

    Create rugs-brends-parse.php

---
+++ rugs-brends-parse.php
@@ -0,0 +1,87 @@
+<?php
+// init library
+require_once '../curl/curl.php';  // https://github.com/shuber/curl
+require_once '../phpquery\phpQuery\phpQuery.php';  // https://github.com/TobiaszCudnik/phpquery
+
+// create instance Curl
+$curl = new Curl;
+
+// get page catalog
+$response = $curl->get('avtopilot1.ru/mates/');
+
+// create new document catalog page
+$doc = phpQuery::newDocument($response->body);
+
+// parse catalog page
+$brends = $doc->find('.tiles .item');
+foreach ($brends as $brend) {  // get links for models
+    $brend_item = pq($brend); // parse element
+
+    $brend_item_title = $brend_item->find('h2')->text();
+    $brend_item_img = $brend_item->find('.preview img')->attr('src');
+    $brend_item_url = 'https://avtopilot1.ru' . $brend_item->attr('href');
+
+    $brend_path = 'img/' . $brend_item_title;
+    if (!is_dir($brend_path) ) {  // check is dir exist
+        mkdir($brend_path, 0755, true);
+    }
+
+    $brend_item_img_extension = parse_url('https://avtopilot1.ru/' . $brend_item_img, PHP_URL_PATH);
+    $brend_item_img_extension = pathinfo($brend_item_img_extension, PATHINFO_EXTENSION);
+
+    $brend_item_img_local_path = $brend_path . '/logo.' . $brend_item_img_extension;
+    if( !file_exists($brend_item_img_local_path) ) {
+        file_put_contents($brend_item_img_local_path, file_get_contents('https://avtopilot1.ru/' . $brend_item_img));
+    }
+
+    $response_models = $curl->get($brend_item_url);
+    $doc_models = phpQuery::newDocument($response_models->body);
+    $models = $doc_models->find('.tiles .item');
+    foreach ($models as $model) {
+        $model_item = pq($model);
+        $model_item_title = $model_item->find('h2')->text();
+        $model_item_img = 'https://avtopilot1.ru' . $model_item->find('.preview img')->attr('src');
+        $model_item_price = $model_item->find('.info .price')->text();
+        $model_item_url = 'https://avtopilot1.ru' . $model_item->attr('href');
+
+
+        $model_path = 'img/' . $brend_item_title . '/' . $model_item_title;
+        $model_path = trim($model_path, '.');
+        if (!is_dir($model_path) ) {  // check is dir exist
+            mkdir($model_path, 0755, true);
+        }
+        $model_item_img_extension = parse_url($model_item_img, PHP_URL_PATH);
+        $model_item_img_extension = pathinfo($model_item_img_extension, PATHINFO_EXTENSION);
+
+        $model_item_img_local_path = $model_path . '/logo.' . $model_item_img_extension;
+        if( !file_exists($model_item_img_local_path) ) {
+            file_put_contents($model_item_img_local_path, file_get_contents($model_item_img));
+        }
+
+        $response_rugs = $curl->get($model_item_url);
+        $doc_rugs = phpQuery::newDocument($response_rugs->body);
+        $rugs = $doc_rugs->find('.colors input');
+        foreach ($rugs as $rug) {
+            $rug_item = pq($rug);
+            $rug_item_id = $rug_item->attr('id');
+            $rug_item_color = $rug_item->attr('value');
+            $rug_item_img_big = 'https://avtopilot1.ru' . $rug_item->attr('data-image');
+            $rug_item_img_small = 'https://avtopilot1.ru' . $doc_rugs->find('.colors label[for='. $rug_item_id .'] img')->attr('src');
+
+            $rug_item_img_big_extension = parse_url($rug_item_img_big, PHP_URL_PATH);
+            $rug_item_img_big_extension = pathinfo($rug_item_img_big_extension, PATHINFO_EXTENSION);
+            $rug_item_img_big_local_path = $model_path . '/' . 'big-' . $rug_item_color . '.' .$rug_item_img_big_extension;
+            if( !file_exists($rug_item_img_big_local_path) ) {
+                file_put_contents($rug_item_img_big_local_path, file_get_contents($rug_item_img_big));
+            }
+
+            $rug_item_img_small_extension = parse_url($rug_item_img_small, PHP_URL_PATH);
+            $rug_item_img_small_extension = pathinfo($rug_item_img_small_extension, PATHINFO_EXTENSION);
+            $rug_item_img_small_local_path = $model_path . '/' . 'small-' . $rug_item_color . '.' .$rug_item_img_small_extension;
+            if( !file_exists($rug_item_img_small_local_path) ) {
+                file_put_contents($rug_item_img_small_local_path, file_get_contents($rug_item_img_small));
+            }
+        }
+    }
+}
+
